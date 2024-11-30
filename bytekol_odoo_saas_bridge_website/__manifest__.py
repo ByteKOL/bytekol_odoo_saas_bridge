7776fef84eaa5336a06851d1e2260301e634be6a
shape-of-snowflake <126655508+shape-of-snowflake@users.noreply.github.com>
2024-11-30 22:06:41 +0700

    [ADD] bytekol_odoo_saas_bridge_website: allow limiting websites (#1)

---
+++ bytekol_odoo_saas_bridge_website/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Bytekol Odoo SaaS Bridge - Website',
+    'description': "Bytekol Odoo SaaS Bridge - Website",
+    'version': '1.0.0',
+    'category': "Tools",
+    'depends': ['website', 'bytekol_odoo_saas_bridge'],
+    'data': [
+    ],
+    'assets': {
+    },
+    'installable': True,
+    'auto_install': True,
+    'license': 'OPL-1',
+}
