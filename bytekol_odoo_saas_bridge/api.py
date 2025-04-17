78d43ef481f5881d99d375a047c155b7e7659c46
shape-of-snowflake <dotd26hp@gmail.com>
2025-04-17 21:10:50 +0700

    [IMP] *: improve saas bridge, add some api

--- bytekol_odoo_saas_bridge/api.py
+++ bytekol_odoo_saas_bridge/api.py
@@ -54,13 +54,21 @@ def bk_api(purpose='api_general', log_traceback=True, custom_response=False, tok
 
                 bk_token = request.env['bk.token'].sudo().ensure_token_valid(token, purpose)
                 request.update_env(user=bk_token.user_id.id)
+                request.env.registry.clear_cache()
                 if api_kwargs.get('one_time_token'):
                     bk_token.sudo().unlink()
 
                 res = func(*args, **kwargs)
                 if custom_response:
                     return res
-                response_data['data'] = res
+                if isinstance(res, (int, dict, float, list, str, type(None))):
+                    response_data['data'] = res
+                elif isinstance(res, OdooResponse):
+                    try:
+                        response_data['data'] = json.loads(res.data)
+                    except Exception:
+                        _logger.warning(f'Cannot loads response {res.data}')
+
             except TokenException:
                 _update_response_error('Token Invalid (X-Authorization-Token).')
             except UserError as e:  # include ValidationError, CacheMiss, ...
--- bytekol_odoo_saas_bridge/controllers/main.py
+++ bytekol_odoo_saas_bridge/controllers/main.py
@@ -9,9 +9,10 @@ import traceback
 import werkzeug
 
 import odoo
+from odoo import _
 from odoo.addons.bytekol_odoo_saas_bridge import api
 from odoo.addons.bytekol_odoo_saas_bridge.db import dump_db
-from odoo.exceptions import AccessDenied
+from odoo.exceptions import AccessDenied, UserError
 from odoo.http import route, request, Controller, content_disposition
 from odoo.modules.registry import Registry
 from odoo.service import security
@@ -28,6 +29,17 @@ class Main(Controller):
         uid = request.session.uid = odoo.SUPERUSER_ID
         request.env.registry.clear_cache()
         request.session.session_token = security.compute_session_token(request.session, request.env)
+        return request.redirect(_get_login_redirect_url(uid))
+
+    @api.bk_api('odoo_saas_api', custom_response=True, one_time_token=True, token_on='url_params')
+    @route('/saas_bridge_user_login', auth='public', methods=['GET'], type='http')
+    def saas_bridge_user_login(self, **kwargs):
+        user = request.env['res.users'].browse(int(kwargs['user_id']))
+        uid = request.session.uid = int(kwargs['user_id'])
+        request.env.registry.clear_cache()
+        request.session.session_token = security.compute_session_token(request.session, request.env)
+        if user.sudo().has_groups('base.group_portal'):
+            return request.redirect('/')
         return request.redirect(_get_login_redirect_url(uid))
 
     @route('/default_admin_login', auth='public', methods=['GET'])
@@ -96,3 +108,59 @@ class Main(Controller):
     @route('/update_app_list', type='http', auth='public', methods=['POST'], csrf=False)
     def update_app_list(self):
         request.env['ir.module.module'].sudo().update_list()
+
+    @api.bk_api('odoo_saas_api', one_time_token=True)
+    @route('/saas_create_user', type='http', methods=['POST'], auth='public', csrf=False)
+    def create_user(self):
+        data = request.get_json_data()
+        user = request.env['res.users'].sudo().create({
+            'name': data['name'],
+            'login': data['login'],
+            'password': data['password'],
+            'groups_id': request.env.ref(data['group_id']).ids,
+        })
+        return json.dumps({'user_id': user.id})
+
+    @api.bk_api('odoo_saas_api', one_time_token=True)
+    @route('/saas_bridge_rpc', type='http', methods=['POST'], auth='public', csrf=False)
+    def saas_bridge_rpc(self):
+        data = request.get_json_data()
+        required_keys = ['model', 'method', 'record_ids']
+        for key in required_keys:
+            if key not in data:
+                raise UserError(_("Required: %s") % key)
+
+        res_ids = []
+        for res_id in data['record_ids']:
+            _id = res_id
+            if isinstance(_id, str):
+                _id = request.env.ref(_id).id
+            res_ids.append(_id)
+
+        records = request.env[data['model']].browse(res_ids)
+        args = data.get('args', [])
+        kwargs = data.get('kwargs', {})
+        if not data.get('ignore_log_kwargs'):
+            _logger.info(
+                f'SaaS RPC: model: {data["model"]}, method: {data["method"]}, record_ids: {data["record_ids"]},' +
+                f' args: {args}, kwargs: {kwargs}'
+            )
+        res = getattr(records.sudo(), data['method'])(*args, **kwargs)
+        return json.dumps(res)
+
+    @api.bk_api('odoo_saas_api', one_time_token=True)
+    @route('/saas_bride_exec_code', type='http', methods=['POST'], auth='public', csrf=False)
+    def saas_bride_exec_code(self):
+        data = request.get_json_data()
+        cron = request.env['ir.cron'].create({
+            'name': f'SaaS Bridge Exec code, time: {odoo.fields.Datetime.now()}',
+            'code': data['code'],
+            'active': False,
+            'user_id': odoo.SUPERUSER_ID,
+            'model_id': request.env.ref('base.model_res_partner').id,
+            'state': 'code',
+        })
+        if not data.get('ignore_log_kwargs'):
+            _logger.info(f'Exec_Code: {data["code"]}')
+        cron.method_direct_trigger()
+        cron.unlink()
