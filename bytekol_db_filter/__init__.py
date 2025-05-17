1b1c1abb69ac5d082a140da666c1a3ce1a295767
shape-of-snowflake <dotd26hp@gmail.com>
2025-05-17 13:58:48 +0700

    [IMP] improve db filter

--- bytekol_db_filter/__init__.py
+++ bytekol_db_filter/__init__.py
@@ -10,13 +10,13 @@ _logger = logging.getLogger(__name__)
 db_filter_origin = http.db_filter
 
 
-def db_filter(dbs, httprequest=None):
-    dbs = db_filter_origin(dbs, httprequest)
-    httprequest = httprequest or http.request.httprequest
+def db_filter(dbs, host=None):
+    httprequest = http.request.httprequest
     db_filter_hdr = httprequest.environ.get("HTTP_X_ODOO_DBFILTER")
     if db_filter_hdr:
-        dbs = [db for db in dbs if re.match(db_filter_hdr, db)]
-    return dbs
+        return [db_filter_hdr]
+    else:
+        return db_filter_origin(dbs, host)
 
 
 http.db_filter = db_filter
